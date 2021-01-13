765431212c888432af9084716a42aa672fc9fecc
Rakesh Kumar <111701024@smail.iitpkd.ac.in>
2021-01-13 20:05:10 +0530

    Add exception handlers

--- client/sender.py
+++ client/sender.py
@@ -3,7 +3,7 @@ class Sender:
     Makes a http request with server
     Request can be GET or POST
     """
-    from requests import get, post
+    from requests import get, post, exceptions
     
     def __init__(self, address):
         self.addr = address
@@ -15,21 +15,29 @@ class Sender:
         """
         Make GET or POST request with dictionary data
         By default, makes a GET request
+        @return True if successful else False
         """
 
         data = kwargs.get('data')
         if data is None:
             # 'No data to send!!!'
-            return
+            return False
 
+        success = False
         # self.post or self.get produces error
         # Use class name instead
-        if kwargs.get('request') == 'post':
-            self.resp = Sender.post(self.addr, data=data)
-        else:
-            self.resp = Sender.get(self.addr, params=data)
+        try:
+            if kwargs.get('request') == 'post':
+                self.resp = Sender.post(self.addr, data=data)
+            else:
+                self.resp = Sender.get(self.addr, params=data)
+            success = True
+        except Sender.exceptions.ConnectionError:
+            print("Connection Refused Error!")
+        except:
+            print("Some error occured!")
 
-        return
+        return success
 
     def get_resp_status(self):
         """
