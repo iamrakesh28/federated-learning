768fbb8ddaffbe2a2631f6fa382c7087ff03bd70
Rakesh Kumar <111701024@smail.iitpkd.ac.in>
2021-01-17 02:18:39 +0530

    Add testing file for model accuracy

---
+++ server/test.py
@@ -0,0 +1,35 @@
+import utility
+import helper
+import pickle
+import numpy as np
+
+def load_datasets(path, filenames):
+
+    X = np.load(path + filenames[0])
+    Y = np.load(path + filenames[1])
+
+    return (X, Y)
+
+def test(filename):
+
+    fp = open(filename, 'rb')
+    data = pickle.load(fp)
+    fp.close()
+    
+    weights = data.get('weights')
+    weights = helper.lists_toarray(weights)
+
+    model = utility.getNewModel()
+    model.set_weights(weights)
+
+    X, Y = load_datasets(
+        "../",
+        ["mnist-train-images.npy", "mnist-train-labels.npy"]
+    )
+
+    pred = utility.predict(model, X[:50])
+    print(np.argmax(pred, axis=1), Y[:50])
+    print(sum(np.argmax(pred, axis=1) == Y[:50]) / Y[:50].shape[0])
+
+if __name__ == "__main__":
+    test('model')
