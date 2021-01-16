0f74c900240737c8b4d378fe3ed03f353c64c8f4
Rakesh Kumar <111701024@smail.iitpkd.ac.in>
2021-01-16 12:25:45 +0530

    Change import statements style

---
+++ client/utility.py
@@ -0,0 +1,62 @@
+from tensorflow.keras import Sequential
+from tensorflow.keras.layers import Flatten, Dense, Activation
+
+# Constants
+
+NUM_IMAGES_TRAIN = 5000
+NUM_CLASSES = 10
+IMAGE_SHAPE = (28, 28)
+
+# Provided Functions
+
+def getNewModel():
+    '''
+    @returns Get Tensorflow based MNIST classifier
+    '''
+    model = Sequential()
+    model.add(Flatten())
+    model.add(Dense(200))
+    model.add(Activation("relu"))
+    model.add(Dense(200))
+    model.add(Activation("relu"))
+    model.add(Dense(NUM_CLASSES))
+    model.add(Activation("softmax"))
+
+    model.build(input_shape=(NUM_IMAGES_TRAIN, IMAGE_SHAPE[0], IMAGE_SHAPE[1]))
+    
+    return model
+
+def predict(model, predData):
+    '''
+    @param model The MNIST classifier
+    @param predData Data to predict on - numpy array of images
+    it has shape (number of images, height of image, width of image)
+    @returns Prediction for each image - it is a numpy array of 
+    shape (number of images,)
+    '''
+    
+    return model.predict(predData)
+
+def trainOnData(model, trainData):
+    '''
+    @param model The MNIST classifier
+    @param trainData Data to train the model on - tuple
+    (numpy array of images, numpy array of labels), the numpy array of images
+    has shape (number of images, height of image, width of image), the numpy
+    array of labels has shape (number of images,)
+    @returns None
+    '''
+
+    model.compile(
+        optimizer=tf.keras.optimizers.Adam(0.001),
+        loss=tf.keras.losses.SparseCategoricalCrossentropy(),
+        metrics=[tf.keras.metrics.SparseCategoricalAccuracy()],
+    )
+
+    (X, y) = trainData
+
+    model.fit(
+        X,
+        y,
+        epochs=20
+    )
